807d8f71892eb7d871abb1c6ca98d4f3cd6fb267
KarinWegner <karinwegner00@gmail.com>
2025-01-08 15:58:41 +0100

    Made navbar for teachers only Moving login to navbar

--- LMS.API/Program.cs
+++ LMS.API/Program.cs
@@ -51,7 +51,7 @@ public class Program
         {
             app.UseSwagger();
             app.UseSwaggerUI();
-            await app.SeedDataAsync();
+           // await app.SeedDataAsync();
         }
 
         app.UseHttpsRedirection();
