c58b8ed9d1db5287f4cb24e719ee0fef694d2b0a
KarinWegner <karinwegner00@gmail.com>
2025-01-09 09:10:59 +0100

    Account button WIP

---
+++ LMS.Blazor/Components/Layout/MainLayout.Razor.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace LMS.Blazor.Components.Layout
+{
+    public partial class MainLayout
+    {
+        [Parameter]
+        public string DisplayName { get; set; } = string.Empty;
+
+
+
+            
+        public MockUser mockUser { get; set; }
+        
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+            mockUser = new MockUser(1, "Robert Karlsson");
+            DisplayName = mockUser.Name;
+        }  
+        public class MockUser
+        {
+            public int Id;
+            public string Name;
+            public MockUser(int id, string name)
+            {
+                Id = id;
+                Name = name;
+            }
+        }
+    }
+}
